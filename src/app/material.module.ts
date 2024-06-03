337ab5ff5c328d64c9f301b8805382a2c6cb64b7
mohammad2rghd <mohammad2rmad@gmail.com>
2024-06-04 02:29:21 +0330

    initialization

---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+
+
+@NgModule({
+  declarations: [
+   
+  ],
+  imports: [
+    MatSlideToggleModule
+ 
+  ],
+  exports:[MatSlideToggleModule],
+  providers: [],
+  bootstrap: []
+})
+export class MaterialModule { }
