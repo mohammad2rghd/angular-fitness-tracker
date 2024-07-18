06b4bedf35a1618d97fa4e7d3706e3cfed035f39
mohammad2rghd <mohammad2rmad@gmail.com>
2024-07-18 23:29:40 +0330

    added bootstrap for layouts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatFormFieldModule, MatInputModule],
 })
 export class MaterialModule {}
